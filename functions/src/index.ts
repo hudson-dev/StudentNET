b12b121358a765c2fb77a59e3ccd8189ffd2058c
rotate0 <hudson.n.kim@gmail.com>
2020-04-20 19:29:03 -0700

    update

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,13 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
+import { DocumentBuilder } from 'firebase-functions/lib/providers/firestore';
+//import { DocumentBuilder } from 'firebase-functions/lib/providers/firestore';
+//import { DocumentBuilder } from 'firebase-functions/lib/providers/firestore';
 admin.initializeApp();
 
 const db = admin.firestore();
 const fcm = admin.messaging();
+//var personFrom: string | functions.Change<functions.firestore.DocumentSnapshot>;
 
 export const sendToDevice = functions.firestore
     .document('messages/{groupChatId}/{chatId}/{message}')
@@ -22,13 +26,26 @@ export const sendToDevice = functions.firestore
         console.log(message.idTo);
         console.log(querySnapshot);
 
-        //const personFrom = db.collection('messages').doc(message.idFrom).data();
+
+        // functions.firestore
+        //     .document('messages/{user}').onCreate(async (snapshot: { data: () => any; }) => {
+        //         const personFrom = snapshot.data();
+        //     }
+
+        // var personFromData = personFrom.data();
+        
+        
+        
+        const personFrom = await db.collection('messages').doc(message.idFrom).get();
+        const data = (await personFrom).data; 
+
+        //var name = data.nickname;
 
         const tokens = querySnapshot.docs.map((snap: { id: any; }) => snap.id);
 
         const payload: admin.messaging.MessagingPayload = {
             notification: {
-                title: "New Message",
+                title: "New Message from " + personFrom + "!",
                 body: message.content,
                 icon: 'your-icon-url',
                 click_action: 'FLUTTER_NOTIFICATION_CLICK'
@@ -41,3 +58,11 @@ export const sendToDevice = functions.firestore
             return undefined
         }
     });
+
+
+// export const getPersonFrom = functions.firestore
+//     .document('active/{user}')
+//     .onCreate(async (snapshot: { data: () => any; }) => {
+//         const data = snapshot.data();
+//         personFrom = data.name;
+//     });
